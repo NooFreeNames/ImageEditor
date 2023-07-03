0f775d4b0bb0f7fc2bcafc9d6e8493d15443b5b8
NooFreeNames <89774221+NooFreeNames@users.noreply.github.com>
2023-07-04 00:19:09 +0500

    pingHandler refactoring * variable names changed from w and r to response and request. * Added error message

--- internal/server/server.go
+++ internal/server/server.go
@@ -31,9 +31,9 @@ func Run() {
 // pingHandler is the handler function for the "/ping" URL. It writes
 // the string "PONG" as the response body. Any error that occurs while writing
 // the response is logged.
-func pingHandler(w http.ResponseWriter, r *http.Request) {
-	_, err := w.Write([]byte("PONG"))
+func pingHandler(response http.ResponseWriter, request *http.Request) {
+	_, err := response.Write([]byte("PONG"))
 	if err != nil {
-		log.Println(err)
+		log.Println("Error writing response: ", err)
 	}
 }
