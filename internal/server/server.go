91238476c6450c8940c040a32c534de141664c22
NooFreeNames <89774221+NooFreeNames@users.noreply.github.com>
2023-07-04 00:11:54 +0500

    Added middleware function to log HTTP requests

--- internal/server/server.go
+++ internal/server/server.go
@@ -6,11 +6,20 @@ import (
 	"net/http"
 )
 
+// Middleware function to log HTTP requests
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(response http.ResponseWriter, request *http.Request) {
+			log.Println("Request: ", request.Method, request.URL.Path)
+			handler.ServeHTTP(response, request)
+		})
+}
+
 // Function Run starts the server and listens for incoming HTTP requests.
 func Run() {
 	fs := http.FileServer(http.Dir("web"))
-	http.Handle("/", fs)
-	http.HandleFunc("/ping", pingHandler)
+	http.Handle("/", logRequest(fs))
+	http.Handle("/ping", logRequest(http.HandlerFunc(pingHandler)))
 
 	log.Println("Server is listening...")
 	err := http.ListenAndServe(":8080", nil)
